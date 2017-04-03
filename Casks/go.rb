1cfb26265578c32a7f8b21e15cfcf8d3c8f7d5b0
Chulki Lee <chulki.lee@gmail.com>
2017-04-03 10:19:57 -0700

    go: use version in url

--- Casks/go.rb
+++ Casks/go.rb
@@ -3,7 +3,7 @@ cask 'go' do
   sha256 'f9d511eb88baecf8a2e3457bf85eaae73dfb7cade4dd4eaba744947efea586e1'
 
   # storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://storage.googleapis.com/golang/go1.8.darwin-amd64.pkg'
+  url "https://storage.googleapis.com/golang/go#{version}.darwin-amd64.pkg"
   name 'Go'
   homepage 'https://golang.org/'
 
