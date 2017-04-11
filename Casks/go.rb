09e6cc3d1cf35b1d4bf9d2596ca996d8a5d530ab
Chulki Lee <chulki.lee@gmail.com>
2017-04-10 21:49:53 -0700

    go: 1.8.1

--- Casks/go.rb
+++ Casks/go.rb
@@ -1,6 +1,6 @@
 cask 'go' do
-  version '1.8'
-  sha256 'f9d511eb88baecf8a2e3457bf85eaae73dfb7cade4dd4eaba744947efea586e1'
+  version '1.8.1'
+  sha256 'd938193b8b230c23b596f3fbc1125e506c6090d6a7679204a7daeeb64b071b5a'
 
   # storage.googleapis.com was verified as official when first introduced to the cask
   url "https://storage.googleapis.com/golang/go#{version}.darwin-amd64.pkg"
