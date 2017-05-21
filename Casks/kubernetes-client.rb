e52372ef7b4bdd6b972a56fbc5b8f6c1d73a7c69
Chulki Lee <chulki.lee@gmail.com>
2017-05-21 21:09:51 +0900

    kubernetes-client: 1.6.4

--- Casks/kubernetes-client.rb
+++ Casks/kubernetes-client.rb
@@ -1,6 +1,6 @@
 cask 'kubernetes-client' do
-  version '1.6.2'
-  sha256 'e3dad0848b3d6c1737199d0704c692e74bf979e6a81fabea79c5b2499ca3fa73'
+  version '1.6.4'
+  sha256 '0d49df4b06f76b5a6e168f72ac0088194d4267cc888880f7d0f23e558cd0ee32'
 
   # dl.k8s.io was verified as official when first introduced to the cask
   url "https://dl.k8s.io/v#{version}/kubernetes-client-darwin-amd64.tar.gz"
