a962304eb5522c174a05e242bea41c25d9bd8424
Chulki Lee <chulki.lee@gmail.com>
2017-06-05 09:28:02 +0900

    minikube: 0.19.1

--- Casks/minikube.rb
+++ Casks/minikube.rb
@@ -1,6 +1,6 @@
 cask 'minikube' do
-  version '0.19.0'
-  sha256 'efe27dd4f791403b90eefab8c1f6ea3c7f3115a7609f41e3189517f088e20f0e'
+  version '0.19.1'
+  sha256 '1689ff1686c07da8b72a4dd2d472ec4d6cf3347eb33804024bb368f455fb4214'
 
   url "https://github.com/kubernetes/minikube/releases/download/v#{version}/minikube-darwin-amd64"
   name 'minikube'
