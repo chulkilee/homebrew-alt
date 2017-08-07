714f11043ec52c4cb5ecb47c2f947f7a82cfe31b
Chulki Lee <chulki.lee@gmail.com>
2017-08-07 13:26:20 -0700

    terraform: 0.10.0

--- Casks/terraform.rb
+++ Casks/terraform.rb
@@ -1,6 +1,6 @@
 cask 'terraform' do
-  version '0.9.6'
-  sha256 '71f53879c2fc33af57238cdb67a344d576ae3ae88f8db112122d433bd762788d'
+  version '0.10.0'
+  sha256 '1584dc21ad5ac1dc0d9a2876542a85d092778d00a0622622c28f8740abadddb9'
 
   # releases.hashicorp.com was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_darwin_amd64.zip"
