1fc9960ca885ef53ea059820835d17897b11444d
Chulki Lee <chulki.lee@gmail.com>
2017-08-07 13:27:27 -0700

    vault: 0.7.3

--- Casks/vault.rb
+++ Casks/vault.rb
@@ -1,6 +1,6 @@
 cask 'vault' do
-  version '0.7.2'
-  sha256 '1b29fabd094ab1afbb26ec6c3df3f7c3deef5ef408bb24db94ae07a330ab5317'
+  version '0.7.3'
+  sha256 'fa9badde19ba0bb9d7341ac0521593c09ea15a00ed824382c1f58855d2e0959c'
 
   # releases.hashicorp.com was verified as official when first introduced to the cask
   url "https://releases.hashicorp.com/vault/#{version}/vault_#{version}_darwin_amd64.zip"
